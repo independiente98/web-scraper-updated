c879046aa85fb8e2e0ca929b266f2ea7e4a63e65
Leti <lgsainz15@gmail.com>
2019-10-07 16:43:28 -0400

    updated bar charts

--- stats_scraper.py
+++ stats_scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as soup
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 # This program uses BeautifulSoup to scrape foxsports for soccer stats and export
@@ -38,6 +39,21 @@ f2.write(headers)
 name_list = []
 value_list = []
 
+
+# create bar charts 
+def plot_graph():
+    y_pos = np.arange(len(name_list))
+    plt.bar(y_pos, value_list)
+    plt.xticks(y_pos, name_list, rotation='82.5')
+    plt.title(title_container.text)
+    plt.tight_layout()
+    # save graph
+    if not os.path.exists("images"):
+        os.mkdir("images")
+    plt.savefig(f'images/{title_container.text}')
+    plt.close()
+
+
 # method to find player info within container and write to csv
 def findPlayer(filename):
     player_container = container.findAll("tr")
@@ -51,23 +67,19 @@ def findPlayer(filename):
         name_list.append(name.text)
         value_list.append(value.text)
 
+    name_list.reverse()
+    value_list.reverse()
+
     # create bar charts
-    plt.bar(name_list, value_list)
-    plt.title(title_container.text)
-    plt.xticks(rotation='82.5')
-    plt.tight_layout()
-    # save graph
-    if not os.path.exists("images"):
-        os.mkdir("images")
-    plt.savefig(f'images/{title_container.text}')
-    plt.close()
+    plot_graph()
 
     # clear list for next function call
     del name_list[:]
     del value_list[:]
     filename.close()
 
-# loop through the "leaders" containers
+
+# loop through the "leaders" containers to make csv/imgs
 for container in containers:
     title_container = container.find("span", {"class":"wisbb_leaderTitle"})
 
